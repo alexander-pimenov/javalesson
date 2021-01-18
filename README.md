dfa71fdd43e603debd39c79ad4cfe0650e6b3b8b
Alexander Pimenov <pimalex1978@yandex.ru>
2021-01-18 16:20:13 +0300

    создания EmailNotification

--- README.md
+++ README.md
@@ -29,6 +29,16 @@ First Header | Second Header
 Content cell 1 | Content cell 2
 Content column 1 | Content column 2
 
+## Topic and Queue JMS
+Simple implementation async 
+- point-to-point (queue)  
+- provider-subscriber (topic)
+
+
+### Helper recourses:
+- [5 Minutes or Less: ActiveMQ with JMS Queues and Topics](https://www.tomitribe.com/blog/5-minutes-or-less-activemq-with-jms-queues-and-topics/)
+- [JMS: Введение в технологию и установка ActiveMQ сервера.](https://onedeveloper.javadev.ru/article%3Fid=12.html)
+
 
 
 
---
+++ chapter_multithreading/src/main/java/ru/pimalex1978/concurrent/pools/email/EmailNotification.java
@@ -0,0 +1,62 @@
+package ru.pimalex1978.concurrent.pools.email;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/*
+ * В этой программе реализовано задание.
+ *
+ * 1. Реализовать сервис для рассылки почты. Создайте класс EmailNotification (Уведомление по электронной почте).
+ * 2. В классе будет метод emailTo(User user) - он должен через ExecutorService отправлять почту.
+ *    Так же добавьте метод close() - он должен закрыть pool.
+ *    То есть в классе EmailNotification должно быть поле pool, которые используется в emailTo() и close().
+ * 3. Модель User описывают поля username, email.
+ * 4. Метод emailTo() должен брать данные пользователя и подставлять в шаблон
+ *      subject = Notification {username} to email {email}.
+ *      body = Add a new event to {username}
+ * 5. создайте метод public void send(String subject, String body, String email) - он должен быть пустой.
+ * 6. Через ExecutorService создайте задачу, которая будет создавать данные для пользователя и передавать
+ *    их в метод send().
+ *
+ * */
+public class EmailNotification {
+    private ExecutorService executorService = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        executorService.submit(() ->
+        {
+            System.out.println("Я в объекте Runnable"); //лог
+            final String subject = String.format("Notification {%s} to email {%S}", user.getUsername(), user.getEmail());
+            final String body = String.format("Add a new event to {%s}", user.getUsername());
+
+            send(subject, body, user.getEmail());
+        });
+    }
+
+    public void close() {
+        System.out.println("Я в close()"); //как лог
+        this.executorService.shutdown();
+        while (!executorService.isTerminated()) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+        System.out.println("Я в методе send()"); //как лог
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setUsername("Alex");
+        user.setEmail("6862613@gmail.com");
+
+        EmailNotification notification = new EmailNotification();
+        notification.emailTo(user);
+        notification.close();
+    }
+}
