a1f09c67b102008bf8c727f95d22a04cb204489e
Alexander Pimenov <alexander_pimenov@epam.com>
2021-12-15 19:12:26 +0300

    добавлен пример ассинхронного выполнения задачи (подсчет факториала)

---
+++ chapter_multithreading/src/main/java/ru/pimalex1978/concurrent/async/JavaAsync.java
@@ -0,0 +1,86 @@
+package ru.pimalex1978.concurrent.async;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * https://www.baeldung.com/java-asynchronous-programming
+ * https://github.com/eugenp/tutorials/blob/master/core-java-modules/core-java-concurrency-advanced-3/src/main/java/com/baeldung/async/JavaAsync.java
+ */
+public class JavaAsync {
+
+    private static final ExecutorService threadpool = Executors.newCachedThreadPool();
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        int number = 20;
+
+        //Thread Example
+        factorialUsingThread(number).start();
+
+        //FutureTask Example
+        Future<Long> futureTask = factorialUsingFutureTask(number);
+        System.out.println("Factorial of " + number + " by 'futureTask' is: " + futureTask.get());
+
+        // CompletableFuture Example
+        Future<Long> completableFuture = factorialUsingCompletableFuture(number);
+        System.out.println("Factorial of " + number + " by 'completableFuture' is: " + completableFuture.get());
+    }
+
+    /**
+     * Finds factorial of a number
+     *
+     * @param number
+     * @return
+     */
+    public static long factorial(int number) {
+        long result = 1;
+        for (int i = number; i > 0; i--) {
+            result *= i;
+        }
+        return result;
+    }
+
+    /**
+     * Finds factorial of a number using Thread
+     *
+     * @param number
+     * @return
+     */
+    public static Thread factorialUsingThread(int number) {
+        Thread newThread = new Thread(() -> {
+            System.out.println("Factorial of " + number + " by 'usingThread' is: " + factorial(number));
+        });
+
+        return newThread;
+    }
+
+    /**
+     * Finds factorial of a number using FutureTask
+     *
+     * @param number
+     * @return
+     */
+    public static Future<Long> factorialUsingFutureTask(int number) {
+        Future<Long> futureTask = threadpool.submit(() -> factorial(number));
+
+        while (!futureTask.isDone()) {
+            System.out.println("FutureTask is not finished yet...");
+        }
+
+        return futureTask;
+    }
+
+    /**
+     * Finds factorial of a number using CompletableFuture
+     *
+     * @param number
+     * @return
+     */
+    public static Future<Long> factorialUsingCompletableFuture(int number) {
+        CompletableFuture<Long> completableFuture = CompletableFuture.supplyAsync(() -> factorial(number));
+        return completableFuture;
+    }
+}
