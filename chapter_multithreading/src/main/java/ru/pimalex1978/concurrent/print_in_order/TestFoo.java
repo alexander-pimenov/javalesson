dcb501d325a6ba1ec510aa6f71718d6e9de4aa85
Alexander Pimenov <alexander_pimenov@epam.com>
2022-06-15 21:53:26 +0300

    добавил коммент

--- chapter_multithreading/src/main/java/ru/pimalex1978/concurrent/print_in_order/TestFoo.java
+++ chapter_multithreading/src/main/java/ru/pimalex1978/concurrent/print_in_order/TestFoo.java
@@ -5,6 +5,9 @@ import static ru.pimalex1978.concurrent.alishev.ColorScheme.CYAN;
 import static ru.pimalex1978.concurrent.alishev.ColorScheme.PURPLE;
 import static ru.pimalex1978.concurrent.alishev.ColorScheme.RED;
 
+/* Асинхронно запускаются три потока. Ввод [1, 2, 3] означает, что поток А вызывает first(),
+ * поток B вызывает second(), C - third().
+ * Должно правильно вывестись "firstsecondthird"*/
 public class TestFoo {
     public static void main(String[] args) {
 //        new Thread(new SimpleRunner()).start();
@@ -12,9 +15,9 @@ public class TestFoo {
         new Thread(() -> {
             try {
                 FooFirstSolution foo = new FooFirstSolution();
-                        foo.first(() -> System.out.println(CYAN + "Hello from first Runnable!"));
-                        foo.second(() -> System.out.println(BLUE + "Hello from second Runnable!"));
-                        foo.third(() -> System.out.println(RED + "Hello from third Runnable!"));
+                foo.first(() -> System.out.println(CYAN + "Hello from first Runnable!"));
+                foo.second(() -> System.out.println(BLUE + "Hello from second Runnable!"));
+                foo.third(() -> System.out.println(RED + "Hello from third Runnable!"));
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
