3ddb00470de0927352c9441ffe3dfba1ea567e3f
LANIT\APimenov <apimenov@lanit.ru>
2023-03-23 10:49:42 +0300

    feat: добавил метод получения из рефлексии названия полей и их значений

--- chapter_reflection/src/main/java/ru/pimalex1978/MyClassRunner.java
+++ chapter_reflection/src/main/java/ru/pimalex1978/MyClassRunner.java
@@ -6,13 +6,16 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * https://javadevblog.com/polnoe-rukovodstvo-po-java-reflection-api-refleksiya-na-primerah.html
+ * <a>https://javadevblog.com/polnoe-rukovodstvo-po-java-reflection-api-refleksiya-na-primerah.html</a>
  * Объект java.lang.Class является точкой входа для всех операций рефлексии. Для каждого типа объекта,
  * JVM создает неизменяемый экземпляр java.lang.Class который предоставляет методы для получения свойств
  * объекта, создания новых объектов, вызова методов.
- *
+ * <p>
  * Разберем что тут сейчас произошло. В java есть замечательный класс Class. Он представляет классы и интерфейсы
  * в исполняемом приложении Java. Связь между Class и ClassLoader мы затрагивать не будем, т.к. это не есть тема статьи.
  * <p>
@@ -27,11 +30,12 @@ import java.lang.reflect.Method;
  * доступ для работы с ним. Метод setAccessible(true) разрешает нам дальнейшую работу. Теперь поле name
  * полностью под нашим контролем! Получить его значение можно вызовом get(Object) у объекта Field, где Object —
  * экземпляр нашего класса MyClass. Приводим к типу String и присваиваем нашей переменной name.
- * 
  */
 public class MyClassRunner {
     @SneakyThrows
     public static void main(String[] args) {
+        AtomicInteger count = new AtomicInteger(1);
+        System.out.println("==( " + count.getAndIncrement() + " )========");
         MyClass myClass1 = new MyClass(10, "Bob");
         int number = myClass1.getNumber();
         String name = null; //no getter =(
@@ -54,7 +58,7 @@ public class MyClassRunner {
             e.printStackTrace();
         }
         System.out.println("hashCode() объекта myClass1 : " + myClass1.hashCode());
-        System.out.println("==========");
+        System.out.println("==( " + count.getAndIncrement() + " )========");
 
         /*
          * На момент старта java приложения далеко не все классы оказываются загруженными
@@ -89,7 +93,7 @@ public class MyClassRunner {
         System.out.println("hashCode() объекта myClass2 : " + myClass2.hashCode());
 
         System.out.println(myClass2); //output created object MyClass{number=0, name='default'}
-        System.out.println("==========");
+        System.out.println("==( " + count.getAndIncrement() + " )========");
 
         /*
          * Как же вызывать методы с аргументами и конструкторы с параметрами?
@@ -106,13 +110,13 @@ public class MyClassRunner {
             myClass3 = (MyClass) clazz3.getConstructor(params).newInstance(1, "default2");
 
         } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException
-                | IllegalAccessException | InvocationTargetException e) {
+                 | IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
         System.out.println("hashCode() объекта myClass3 : " + myClass3.hashCode());
 
         System.out.println(myClass3); //output created object MyClass{number=1, name='default2'}
-        System.out.println("==========");
+        System.out.println("==( " + count.getAndIncrement() + " )========");
 
         MyClass myClass4 = null;
         try {
@@ -128,7 +132,7 @@ public class MyClassRunner {
             e.printStackTrace();
         }
 
-        System.out.println("==========");
+        System.out.println("==( " + count.getAndIncrement() + " )========");
         //Представим, что нам нужно спомощью рефлексии задать другое отличное при создании значение полю number
         MyClass myClassNick = new MyClass(10, "Nick");
         System.out.println(myClassNick); //MyClass{number=10, name='Nick'}
@@ -145,12 +149,72 @@ public class MyClassRunner {
         должен быть экземпляром класса, которому принадлежит само поле.
         В приведенном примере используется экземпляр класса myClassNick, потому что поле
         fieldName является членом экземпляра этого класса.*/
-        fieldNumber.set(myClassNick,25);
+        fieldNumber.set(myClassNick, 25);
         System.out.println(myClassNick); //MyClass{number=25, name='Nick'}
 
-        System.out.println("==========");
+        System.out.println("==( " + count.getAndIncrement() + " )========");
+
+        MyClass myClass = new MyClass(55, "Sash");
+        printFieldsValues(myClass);
+        MyClass myClass5 = new MyClass();
+        printFieldsValues(myClass5);
+        System.out.println("==( " + count.getAndIncrement() + " )========");
+
+        System.out.println(printFieldsValuesAndPutToMap(myClass));
 
 
+    }
 
+    /**
+     * Данный метод принимает на вход объект любого класса и использует Reflection
+     * для получения его полей и их значений. В цикле for происходит перебор всех
+     * полей класса, после чего каждое поле устанавливается доступным к использованию
+     * через метод setAccessible(true). Получение значения поля объекта происходит с
+     * помощью метода get() и выводится на консоль в формате "название поля = значение поля".
+     * Если у поля нет значение по умолчанию, то значение будет null.
+     *
+     * @param object объект любого типа
+     */
+    public static void printFieldsValues(Object object) {
+        Class<?> clazz = object.getClass();
+        // Получаем все поля класса, включая наследуемые от родительских классов
+        Field[] fields = clazz.getDeclaredFields();
+
+        for (Field field : fields) {
+            try {
+                // Устанавливаем доступ к приватному полю,
+                // если оно не было установлено при объявлении
+                field.setAccessible(true);
+                // Получаем значение поля объекта
+                Object value = field.get(object);
+                // Выводим название поля и его значение
+                System.out.println(field.getName() + " = " + value);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static Map<String, Object> printFieldsValuesAndPutToMap(Object object) {
+        Map<String, Object> result = new HashMap<>();
+        Class<?> clazz = object.getClass();
+        // Получаем все поля класса, включая наследуемые от родительских классов
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            try {
+                // Устанавливаем доступ к приватному полю,
+                // если оно не было установлено при объявлении
+                field.setAccessible(true);
+                // Получаем значение поля объекта
+                Object value = field.get(object);
+                // Можем вывести название поля и его значение
+                System.out.println(field.getName() + " = " + value);
+                // Помещаем результат в мапу
+                result.put(field.getName(), value);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 }
