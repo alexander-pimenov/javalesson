c205b857394118ebcac91a2b2be171cdfc270d77
Alexander Pimenov <alexander.pimenov.s@yandex.ru>
2023-03-30 21:51:41 +0300

    feat: добавил trim и split строки

--- chapter_io/src/main/java/ru/pimalex1978/Test.java
+++ chapter_io/src/main/java/ru/pimalex1978/Test.java
@@ -2,6 +2,7 @@ package ru.pimalex1978;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.UUID;
 
 public class Test {
 
@@ -18,4 +19,43 @@ public class Test {
 //            }
 //        }
 //    }
+
+    public static void main(String[] args) {
+        String s = " fkfkfk";
+        String trim = s.trim();
+        String[] s1 = trim.split(" ");
+        if (s1.length == 0) {
+            System.out.println("пусто");
+        } else {
+            for (int i = 0; i < s1.length; i++) {
+                System.out.println(i + " " + s1[i]);
+            }
+            System.out.println("размер массива = " + s1.length);
+        }
+
+        System.out.println(UUID.randomUUID());
+        System.out.println(UUID.randomUUID());
+        System.out.println(UUID.randomUUID());
+        System.out.println(UUID.randomUUID());
+    }
 }
+
+//ввод:
+//3 3 12
+//DISABLE 1 2
+//DISABLE 2 1
+//DISABLE 3 3
+//GETMAX
+//RESET 1
+//RESET 2
+//DISABLE 1 2
+//DISABLE 1 3
+//DISABLE 2 2
+//GETMAX
+//RESET 3
+//GETMIN
+
+//вывод:
+//1
+//2
+//1
