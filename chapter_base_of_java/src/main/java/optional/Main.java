27b52d8c73f7e821cae3b6dcbf732b9386f93ba9
Alexander Pimenov <alexander.pimenov.s@yandex.ru>
2023-05-05 11:24:51 +0300

    refact: добавил комменты для Optional

--- chapter_base_of_java/src/main/java/optional/Main.java
+++ chapter_base_of_java/src/main/java/optional/Main.java
@@ -12,38 +12,38 @@ import java.util.Optional;
 public class Main {
     public static void main(String[] args) {
         //3 способа создания Optional
-        Optional<String> optional = Optional.of("hello");
+        Optional<String> optionalNotEmpty = Optional.of("hello");
         Optional<String> optionalEmpty = Optional.empty();
         Optional<String> optionalOfNullable = Optional.ofNullable("Hi");
 
         //Если значение присутствует, выполняет данное действие со значением,
         // т.е. s.length(), в противном случае ничего не делает.
-        System.out.println(optional.isPresent()); // true
-        optional.ifPresent(s -> System.out.println(s.length())); // 5
-        System.out.println(optional.get()); // "hello"
-        optional.orElse("ops...");
+        System.out.println(optionalNotEmpty.isPresent()); // true
+        optionalNotEmpty.ifPresent(s -> System.out.println("длина слова = " + s.length())); // 5
+        System.out.println(optionalNotEmpty.get()); // "hello"
+        System.out.println(optionalNotEmpty.orElse("ops...")); // "hello" - выполнится get(), т.к. есть значение
+        System.out.println(optionalOfNullable.orElse("ops...")); // "Hi"
 
         System.out.println(optionalEmpty.isPresent()); // false
-        optionalEmpty.ifPresent(s -> System.out.println(s.length())); // 0
-//        System.out.println(optionalEmpty.get()); // "hello"
-        String orElse = optionalEmpty.orElse("ops...");
-        System.out.println(orElse);//ops...
+        optionalEmpty.ifPresent(s -> System.out.println("длина слова = " + s.length())); // это действие не выполнится, т.к. optionalEmpty
+        //System.out.println(optionalEmpty.get()); // будет ошибка 'java.util.NoSuchElementException: No value present', т.к. optionalEmpty
+        System.out.println(optionalEmpty.orElse("ops..."));//ops...
 
         /* Метод orElseGet() - если значение присутствует, возвращает значение, в противном
          * случае возвращает результат, полученный функцией-поставщиком.
          * Обрати внимание, что возвращается Стринг, т.е. тот тип которым параметризован Optional!
          * Броски исключений:
          *         NullPointerException — если значение отсутствует, а функция-поставщик равна нулю.*/
-        String orElseGet = optionalEmpty.orElseGet(() -> "ops from Supplier");//
-        System.out.println(orElseGet);
+        System.out.println(optionalEmpty.orElseGet(() -> "ops from Supplier")); //"ops from Supplier"
+        System.out.println(optionalOfNullable.orElseGet(() -> "ops from Supplier")); //Hi
 
-        //Можно и так возвращать, что нибудь))))
+        //Можно и так возвращать, что нибудь, но совместимым с параметризацией optional, в нашем случае это String))))
         String userString = optionalEmpty.orElseGet(() -> {
             return new User("Пустой юзер").toString();
         });
         System.out.println(userString);
 
-        String orElseGet2 = optional.orElseGet(() -> "ops from Supplier");//
+        String orElseGet2 = optionalNotEmpty.orElseGet(() -> "ops from Supplier");//
         System.out.println(orElseGet2);
     }
 }
