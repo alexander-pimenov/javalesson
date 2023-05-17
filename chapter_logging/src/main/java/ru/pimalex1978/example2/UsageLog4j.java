1b69881906e26b7fa8810311331aa3bcca4dcb65
Alexander Pimenov <alexander.pimenov.s@yandex.ru>
2023-05-17 14:29:52 +0300

    refact: исправил комменты

--- chapter_logging/src/main/java/ru/pimalex1978/example2/UsageLog4j.java
+++ chapter_logging/src/main/java/ru/pimalex1978/example2/UsageLog4j.java
@@ -11,10 +11,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 /**
- * Хорошее обучающее видео о логгировании https://www.youtube.com/watch?v=N4fmELeXw5I
+ * Хорошее обучающее видео о логировании <a href="https://www.youtube.com/watch?v=N4fmELeXw5I">ссылка</a>
  * Выпуск 72. Логирование - важный инструмент для разработчика.
  * Здесь использована связка slf4j и log4j (в pom.xml есть три соответствующие зависимости)
- * В нем рассказывется для чего логгирование и, кратко, об его уровнях: TRACE, INFO, DEBUG, WARN, ERROR.
+ * В нем рассказывется для чего логирование и, кратко, об его уровнях: TRACE, INFO, DEBUG, WARN, ERROR.
  * Здесь взят пример из https://javarush.ru/groups/posts/2293-zachem-nuzhno-logirovanie
  * И некоторая информация от сюда https://javarush.ru/groups/posts/2388-logirovanie-chto-kak-gde-i-chem
  * И от сюда http://skipy.ru/useful/logging.html#uls_slf
@@ -24,10 +24,10 @@ import java.nio.file.Paths;
  * и https://urvanov.ru/2019/07/04/%d0%bb%d0%be%d0%b3%d0%b8%d1%80%d0%be%d0%b2%d0%b0%d0%bd%d0%b8%d0%b5-%d1%81-log4j-2-%d0%b2-java/
  *
  * <p>
- * Этот класс обращается к файлу log4j.properties для правильного вывода информации в консоль,
+ * Этот класс обращается к файлу log4j.properties (log4j.xml) для правильного вывода информации в консоль,
  * * т.е. это файл настройки вывода лога.
  * <p>
- * Касаемо вопроса по-поводу уровней логгирования. То негласные договоренности такие:
+ * Касаемо вопроса по-поводу уровней логирования. То негласные договоренности такие:
  * <p>
  * Уровень INFO - просто информирование о неком событии
  * Уровень DEBUG - используется при отладке
@@ -56,9 +56,9 @@ public class UsageLog4j {
             LOG.trace("trace message. Зашли в блок try-catch");
 
             Files.readAllBytes(Paths.get(FILENAME));
-        } catch (IOException e) {
+        } catch (IOException ex) {
             //Используя org.slf4j.Logger мы можем использовать строку с параметром {}:
-            LOG.error("Error message from catch-block. Failed to read file {}.", FILENAME, e);
+            LOG.error("Error message from catch-block. Failed to read file {}.", FILENAME, ex);
 //            LOG.error(e.getMessage(), e); //удобно искать ошибку в логе
 
             //Используя org.apache.log4j.Logger мы не сможем использовать {}
